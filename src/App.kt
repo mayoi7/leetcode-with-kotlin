3d34abdc990ed4b485a418696f0976dcc9d86a40
liuhaonan <acerola.orion@foxmail.com>
2020-06-18 09:23:52 +0800

    1028. 从先序遍历还原二叉树

--- src/App.kt
+++ src/App.kt
@@ -14,7 +14,7 @@ import kotlin.collections.HashMap
  * https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/
  */
 fun main() {
-    val obj = Codec();
+/*    val obj = Codec()
 
     val root = TreeNode(1)
     root.left = TreeNode(2)
@@ -26,18 +26,23 @@ fun main() {
     println(serialize)
 
     val newRoot = obj.deserialize(serialize)
-    println(obj.serialize(newRoot))
+    println(obj.serialize(newRoot))*/
+
+    val solution = Solution()
+    val str = "1-52--3--4-5--6--7"
+    val root = solution.recoverFromPreorder(str)
+    println(root?.`val`)
 }
 
-class Codec() {
+class Codec {
 
-    private val DELIMITER = "-";
+    private val DELIMITER = "-"
 
     // Encodes a URL to a shortened URL.
     fun serialize(root: TreeNode?): String {
-        var treeString = "";
+        var treeString = ""
         if (root == null) {
-            return "*-";
+            return "*-"
         }
         treeString += "${root.`val`}$DELIMITER"
         treeString += serialize(root.left)
@@ -48,7 +53,7 @@ class Codec() {
     // Decodes your encoded data to tree.
     fun deserialize(data: String): TreeNode? {
         if (data == "") {
-            return null;
+            return null
         }
         val res = ArrayList(data.split("-"))
         res.removeAt(res.size - 1)
@@ -60,7 +65,7 @@ class Codec() {
             res.removeAt(0)
             return null
         }
-        val root = TreeNode(res[0].toInt());
+        val root = TreeNode(res[0].toInt())
         res.removeAt(0)
         root.left = makeTree(res)
         root.right = makeTree(res)
@@ -79,5 +84,70 @@ class Solution {
             left = left.coerceAtLeast(A[i] + i)
         }
         return max
+    }
+
+    // 1028. 从先序遍历还原二叉树
+    fun recoverFromPreorder(S: String): TreeNode? {
+        if (S == "") {
+            return null
+        }
+        val arr = ArrayList<Int>()
+        arr.add(0)
+        for (i in 1 until S.length) {
+            if (S[i] != '-' && S[i - 1] == '-') {
+                // num start
+                arr.add(i)
+            } else if (S[i] == '-' && S[i-1] != '-') {
+                // num end
+                arr.add(i - 1)
+            }
+        }
+        arr.add(S.length - 1)
+        var start = 0
+        var lastEnd = -1
+        val nums = IntArray(arr.size)
+        nums[0] = 0
+        var k = 0
+        for (end in 1 until arr.size step 2) {
+            start = arr[end - 1]
+            nums[k++] = start - lastEnd - 1
+            if (start == arr[end]) {
+                nums[k++] = S[arr[end]] - '0'
+            } else {
+                nums[k++] = S.substring(start, arr[end] + 1).toInt()
+            }
+            lastEnd = arr[end]
+        }
+        val root = TreeNode(nums[1])
+        var node = root
+        var lastDeep = 0
+        var parent = root
+        val stack = LinkedList<TreeNode>()
+        stack.push(root)
+        for (i in 2 until nums.size step 2) {
+            when {
+                nums[i] < lastDeep -> {
+                    for (j in 0 until lastDeep - nums[i] + 1) {
+                        stack.pop()
+                    }
+                    parent = stack.peek()
+                    parent.right = TreeNode(nums[i + 1])
+                    stack.push(parent.right)
+                }
+                nums[i] == lastDeep -> {
+                    stack.pop()
+                    parent = stack.peek()
+                    parent.right = TreeNode(nums[i + 1])
+                    stack.push(parent.right)
+                }
+                else -> {
+                    parent = stack.peek()
+                    parent.left = TreeNode(nums[i + 1])
+                    stack.push(parent.left)
+                }
+            }
+            lastDeep = nums[i]
+        }
+        return root
     }
 }
